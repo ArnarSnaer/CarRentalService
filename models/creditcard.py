2eaffc6429f7852a5030c4224a99940b4544cf41
Bjarni <bjarnib18@ru.is>
2018-12-04 11:51:49 +0000

    First commit

---
+++ models/creditcard.py
@@ -0,0 +1,24 @@
+class Creditcard(object):
+    def __init__(self,name,address,phone,credit,duration,sec_num):
+        self.name = name
+        self.address = address
+        self.phone = phone
+        self.credit = credit
+        self.duration = duration
+        self. sec_num = sec_num
+        self.info = [self.name, self.address,self.phone,self.credit,self.duration,self.sec_num]
+    
+    def __str__(self):
+        return str(self.info)
+    
+    def __getitem__(self,index):
+        return self.info[index]
+    
+    def __iter__(self):
+        return iter(self.info)
+
+#thing = Creditcard("Ari","Gullfoss 2", 5812345, "5555 5555 5555 5555", "10/21", "123")
+#print(thing[3])
+
+#for item in thing:
+ #   print(item)
---
+++ models/employee.py
@@ -0,0 +1,20 @@
+#from Order import Order
+class Employee(object):
+    def __init__(self,name):
+        self.name = []
+        self.name.append(name)
+    
+    def __str__(self):
+        return "{}".format(self.name[1:])
+    
+    def get_name(self):
+        return "{}".format(self.name[0])
+    
+    def add_sale(self,other): #Þetta færir order, gerð bílsins eða bílnumer...
+        return self.name.append(other)
+
+gunnar = Employee("Gunnar")
+gunnar.add_sale("Honda civic 2014")
+print(gunnar)
+nafn = gunnar.get_name()
+print(nafn)
