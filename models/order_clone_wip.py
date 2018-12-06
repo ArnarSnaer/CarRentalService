aa59613ec0936d15292c86ad00d4ed6309432d99
Bjarni <bjarnib18@ru.is>
2018-12-06 14:14:33 +0000

    Tilraun til að sameina skjölin okkar

--- models/order_clone_wip.py
+++ models/order_clone_wip.py
@@ -21,35 +21,32 @@ class Order(object):
         self.min_duration = 1
         self.info= [self.credit_info,date_start,date_end,self.car,self.employee]
 
-        #Þetta reiknar heildarkostnað (total_price)
-        # 1. Reiknar fjölda daga sem pöntunin stendur yfir
-        # 2. Býr til payment og skilartölugildi hennar
+    def get_status(self):
+        return self.car.get_status()
+        #Þarf að impliment-a þetta öðruvísi
+    
+    def get_base_price(self): #laga þegar þetta virkar allt
+        return self.payment.base_price
+
+
+    def calc_duration(self):
         day1,month1,year1 = self.date_start.split(" ")
         day2,month2,year2 = self.date_end.split(" ")
         date1 = datetime(int(year1),int(month1),int(day1))
         date2 = datetime(int(year2),int(month2),int(day2))
         duration = date2 - date1
         days_num = duration.days
+        return days_num
 
-        self.order_payment = Payment(self.client,self.car,days_num)
-        self.total_cost = self.order_payment
-        
+    def create_payment(self):
+        order_payment = Payment(self.client, self.car)
+        return order_payment
 
-    def get_status(self):
-        return self.car.get_status()
-        #Þarf að impliment-a þetta öðruvísi
-    
-    def get_base_price(self):
-        return self.order_payment.base_price
-
-    def calc_duration(self,date_start,date_end):
-        day1,month1,year1 = date_start.split(" ")
-        day2,month2,year2 = date_end.split(" ")
-        date1 = datetime(int(year1),int(month1),int(day1))
-        date2 = datetime(int(year2),int(month2),int(day2))
-        duration = date2 - date1
-        days = duration.days
-        return days
+    def calc_total_cost(self,order_payment,days_num):
+        payment_val = order_payment.calc_rental_cost(days_num,self.car)
+        self.total_cost = self.price + payment_val
+        print("THING: ", self.price, " ", payment_val)
+        return self.total_cost
     
     def add_insurance(self,price,other):
         self.price.add_insurance(other)
@@ -80,13 +77,16 @@ class Order(object):
        # return self.total_cost
 
 
-card = Creditcard("Ari","Gullfoss 2", 5812345, "5555 5555 5555 5555", "10/21", "123")
+card = Creditcard("Ari Halldórsson","Gullfoss 2", 5812345, "5849 9471 0372 0813", "10/21", "123")
 bill = Car("Jeppi","Toyota Land Cruiser","ER C01","4x4",True,False,60000,"diesel")
+bill.check_price()
 vinur = Client("Jón Gústafsson", "Geysir 7", 5885522,"17 Júní", "1234 5678", "USA", "779")
-gunnar = Employee("Gunnar")
+gunnar = Employee("Gunnar Stefánsson")
 
 
-first_order = Order("AC 101",card,"1 1 2014","1 1 2015",bill,vinur,gunnar)
-#first_order.order_payment.add_insurance("t2")
+first_order = Order("A1",card,"1 1 2014","1 6 2014",bill,vinur,gunnar)
+num_of_days = first_order.calc_duration()
+test_payment = first_order.create_payment()
+first_order.calc_total_cost(test_payment,num_of_days)
 print(first_order)
 #def __init__(self,order_id,credit_info, date_start, date_end,car,client,employee):
--- models/payment.py
+++ models/payment.py
@@ -3,26 +3,25 @@ from insurance import Insurance
 from car import Car
 
 class Payment(object):
-    def __init__(self,client,car):
+    def __init__(self,client,car,duration=1):
         self.price = 0
         self.base_price = car.get_price()
         self.client = client
-        self.total_price = self.base_price + self.price
         self.insurances = []
+        self.total_price = (car.price * duration)
         self.car = car
 
-    def calc_rental_cost(self,duration,car):
-        total_price = (car.price * duration) + self.price
-        #total_price = self.price + self.base_price
-        return total_price
+    #def calc_rental_cost(self,duration,car):
+     #   self.total_price = (car.price * duration) + self.price
+      #  return self.total_price
     
     def add_insurance(self,other):
         choice = other
         if choice not in self.insurances:
-            print(choice)
             self.insurances.append(choice)
             print("Insurances: ", self.insurances)
             self.price = int(self.price + Insurance(choice))
+            self.total_price += self.price
             return self.price
         else:
             return "This insurance is already applied to your order"
@@ -34,12 +33,13 @@ class Payment(object):
         return "Payment: {}\nClient: {}".format(str(self.total_price),self.client.get_name())
     
 
-#person = Client("Jón", "Geysir 7", 5885522,"17 Júní", "1234 5678", "USA", "779")
-#car = Car("Jeppi","Toyota Land Cruiser","ER C01","4x4",True,False,60000,"diesel",14000)
-#money = Payment(person,car)
-#print(money)
-#money.add_insurance("t3")
-#print(money)
+person = Client("Jón", "Geysir 7", 5885522,"17 Júní", "1234 5678", "USA", "779")
+car = Car("Jeppi","Toyota Land Cruiser","ER C01","4x4",True,False,60000,"diesel",14000)
+money = Payment(person,car)
+print(money)
+print("")
+money.add_insurance("t3")
+print(money)
 #money.add_insurance("t1")
 #print(money)
 #money.add_insurance("t1")
