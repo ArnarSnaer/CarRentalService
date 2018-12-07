09d06a2b521b68e39810038b41a4485a5de6c553
Bjarni <bjarnib18@ru.is>
2018-12-07 13:24:14 +0000

    Bætti print_GUI.py og bjó til fall (redirect_user.py) sem bendir notendanum þangað sem hann vill fara. Ennþá að eiga við import og skjala uppsettningu

--- functions/lookup_order.py
+++ functions/lookup_order.py
@@ -5,11 +5,11 @@ import datetime
 
 def orders(bilnr, input_start, input_end):
 
-    with open("order.txt", 'r') as test_file:
+    with open("./databases/order.txt", 'r') as test_file:
         is_free = True
         for line in test_file:            
             pn, bn, start, end, name = line.split(',')
-''' datetime.datetime.strptime() breytir "1999 03 07" í datetime instance af 1999-03-07 00:00:00'''
+#datetime.datetime.strptime() breytir "1999 03 07" í datetime instance af 1999-03-07 00:00:00:
             order_start = datetime.datetime.strptime(start, '%Y %m %d')
             order_end = datetime.datetime.strptime(end, '%Y %m %d')
             input_start = datetime.datetime.strptime(input_start, '%Y %m %d')
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from functions.print_GUI import print_GUI
 from functions.user_input import user_input
 from functions.shutting_down import shutting_down
 from functions.lookup_client import lookup_client
+from functions.redirect_user import redirect_user
 
 go_again = True
 
@@ -9,9 +10,8 @@ while go_again:
     print_GUI()
     choice = user_input()
 
-    if choice == "3":
-        lookup_client()
-
     if choice == "0":
         shutting_down()
         go_again = False
+    else:
+        redirect_user(choice)
