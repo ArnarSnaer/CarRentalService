f309de0c004d609d0cb6785955c15bdeeee19139
Bjarni <bjarnib18@ru.is>
2018-12-08 17:20:31 +0000

    kláruðum employee samkvæmt nýju uppsettningunni, datt í hug að allt sem kerfið prentar sé með

---
+++ N E W/Main/employee_ui.py
@@ -0,0 +1,36 @@
+from services.employee_services import Employee_services
+from models.employee import Employee
+
+class Employee_UI(object):
+    def __init__(self,name):
+        self.name = name
+        self.employee_ser = Employee_services(name)
+        self.employee = Employee(name)
+    
+    def print_sales(self):
+        print(self.employee_ser.get_sales())
+
+    def __str__(self):
+        return "{}".format(self.name)
+    
+    def employee_menu(self):
+        choice = ""
+        while(choice != "q"):
+            print("What would you like to do?\n1. Add sale\n2. Remove sale\n3. Get a list of all sales\nq. Quit")
+            choice = input("What would you like to do? ").lower()
+
+            if choice == "1":
+                sale = input("Input license plate of rented car: ")
+                self.employee_ser.add_sale(sale)
+            
+            elif choice == "2":
+                sale = input("Inpur license plate of rented car: ")
+                self.employee_ser.remove_sale(sale)
+            
+            elif choice == "3":
+                self.print_sales()
+            
+            
+
+Gunnar = Employee_UI("Gunnar")
+Gunnar.employee_menu()
--- N E W/Main/services/employee_services.py
+++ N E W/Main/services/employee_services.py
@@ -1,17 +1,41 @@
-from models.employee import 
+from models.employee import Employee
 
 class Employee_services(object):
-    def __init__(self):
-        self.employee = Employee()
+    def __init__(self,name):
+        self.name = name
+        self.employee = Employee(name)
 
-        def add_sale(self,other): #Þetta færir order, gerð bílsins eða bílnumer...
-            return self.name.append(other)
-    
-        def get_sales(self):
-            sales_txt = ""
-            for line in self.employee.name:
-                sales_txt += line
-            
-            print(sales_txt)
+    def add_sale(self,other): #Þetta færir order, gerð bílsins eða bílnumer...
+        try:
+            return self.employee.name.append(other)
+        except ValueError:
+            print("> Sale not found, try again")
+        
 
-Gunnar = Employee("Gunnar")
+    def remove_sale(self,other):
+        try:
+            return self.employee.name.remove(other)
+        except ValueError:
+            print("> Plate not found in sales")
+
+    def get_sales(self):
+        employee_order = ""
+        for item in self.employee.name[1:]:
+            if item != self.employee.name[-1]:
+                employee_order += item + "\n"
+            else:
+                employee_order += item
+
+        if employee_order == "":
+            return "> No sales"
+        else:
+            return employee_order
+        
+
+#Gunnar = Employee_services("Gunnar")
+#Gunnar_v2 = Employee_services()
+#Gunnar.add_sale("Skoda")
+#Gunnar.add_sale("Ferrari")
+#Gunnar.add_sale("Toyota")
+#Gunnar.remove_sale("Ferrari")
+#print(Gunnar.get_sales())
