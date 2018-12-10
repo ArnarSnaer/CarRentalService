9928a2e4c35ca43ca32674aeec47e910900187ae
ArnarSnaer <42970490+ArnarSnaer@users.noreply.github.com>
2018-12-10 22:34:06 +0000

    Endurskrifað car_service.py
    Fór yfir allt til að láta car_ui.py fá fulla virkni. Þarf að fara yfir og hreinsa/fínpússa

--- N E W/Main/services/car_services.py
+++ N E W/Main/services/car_services.py
@@ -1,12 +1,7 @@
 from repositories.car_repo import Car_repository
 #from models.car import Car
 
-
-<<<<<<< HEAD
-class Car_services:
-=======
 class Car_services(object):
->>>>>>> f95854d91ab91f012ba0e676561ba88868bd7ce7
     def __init__(self):
         self.car_repo = Car_repository()
         self.keywords = []
@@ -15,14 +10,23 @@ class Car_services(object):
         created_car = self.car_repo.Car(car_info[0],car_info[1],car_info[2],car_info[3],car_info[4],car_info[5],car_info[6],car_info[7])
         return created_car
 
+    def create_car_from_list(self, car_list):
+        car_info = car_list.split(",")
+        created_car = self.car_repo.Car(car_info[0],car_info[1],car_info[2],car_info[3],car_info[4],car_info[5],car_info[6],car_info[7])
+        return created_car
+
     def list_to_formated_str(self,list):
         result_str = ""
-        for item in list:
-            if len(list) != 0:
-                result_str += "\n"
-            result_str += item
-
-        return result_str
+        if len(list) == 0:
+            return "No matches!"
+        else:
+            for item in list:
+                if "\n" in item:
+                    item = item[:-1]
+                else:
+                    item += ","
+                result_str += item
+            return result_str
     
     def create_keyword_list(self):
         return self.keywords
@@ -31,9 +35,20 @@ class Car_services(object):
         return self.keywords.append(keyword)
     
     def rent_car(self,car_object):
-        if car_object.get_status() == True:
-            car_object.status = False
-            
+        car_object = str(car_object)
+        split_list = car_object.split(",")
+        if split_list[4] == "True":
+            split_list[4] = "False" # Er hægt að stytta, einhver með fleiri brain cells að laga
+        else:
+            split_list[4] = "True"
+        new_str = ""
+        for item in split_list:
+            new_str += item
+            if item == split_list[-1]:
+                new_str += "\n"
+            else:
+                new_str += ","
+        return new_str
 
     def return_car(self,car_object):
         if car_object.get_status() == False:
@@ -52,18 +67,17 @@ class Car_services(object):
     def get_all_cars(self):
         results_txt = ""
         file_text = self.car_repo.get_all_cars()
+        number = 1
         for line in file_text:
-            if line == file_text[-1]:
-                line = line.strip("\n")
-            results_txt += line
+            car = self.create_car_from_list(line)
+            veh_type = self.car_repo.Car.get_veh_type(car)
+            brand = self.car_repo.Car.get_brand(car)
+            plate = self.car_repo.Car.get_plate(car)
+            if self.car_repo.Car.get_status(car) == "True":
+                status = "Available."
+            else:
+                status = "Unavailable."
+            print("{}. Type: {} Brand: {} License plate: {} Current status: {}".format(number,veh_type, brand, plate, status))
+            number += 1
         
         return results_txt
-
-#hér vantar fall til að sjá hvort þessi bíll sé "VALID"
-    def add_car(self, car):
-        self.car_repo.add_car(car)
-    
-    def remove_car(self, car):
-        self.car_repo.remove_car(car)
-    
-    
