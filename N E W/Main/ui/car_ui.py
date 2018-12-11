df8103ac05d7708525f861893148cb362042a25a
ArnarSnaer <42970490+ArnarSnaer@users.noreply.github.com>
2018-12-11 21:17:29 +0000

    Risastór endurgerð á car_ui.py
    Gætur núna bætt við bíl í database, tekið út bíl, fundið bíl eða séð alla bíla. Foolproof-aði að bæta við bíl. Má aðeins fínpússa.

--- N E W/Main/ui/car_ui.py
+++ N E W/Main/ui/car_ui.py
@@ -11,79 +11,133 @@ class Car_UI(object):
         number = 1
         for item in results:
             car_object = self.car_serv.create_car(item)
-            veh_type = self.car_serv.car_repo.car_model.get_veh_type(car_object)
+            veh_type = self.car_repo.car_model.get_veh_type(car_object)
             brand = self.car_repo.car_model.get_brand(car_object)
             plate = self.car_repo.car_model.get_plate(car_object)
             if self.car_repo.car_model.get_status(car_object) == "True":
                 status = "Available."
             else:
                 status = "Unavailable."
-            print("{}. Type: {} Brand: {} License plate: {} Current status: {}".format(number,veh_type, brand, plate, status))
+            print("{}. {} Type: {:>5s} Brand: {} License plate: {} Current status: {}".format(number,"|",veh_type, brand, plate, status))
             number += 1
-        print("Complete! Here are all the results of the search.")
-        choice = int(input("Choose a car: "))
-        index_var = choice-1
-        return results[index_var]
+        print("Complete! Here are all the results of the search.\n")
     
     def car_menu(self):
         choice = ""
 
         while choice != "q":
-            print("Current section: Cars\n1. Rent car\n2. Return car\n3. Search car database\n4. See all cars\nq. Quit")
+            print("\nCurrent section: Cars\n1. Add a new car into the database\n2. Remove a car from the database\n3. Search car database\n4. See all cars in the current database\nq. Quit")
             choice = input("> What would you like to do? ").lower()
-            
+            # {"suv": 100000, "mini": 10000, "mpv": 50000, "sport": 200000,"sedan": 75000}
             if choice == "1":
-                #print(self.car_serv.get_available_cars())
-                plate = input("> Enter the license plate of desired car: ")
-                results = self.car_repo.find_car(plate)
-
-                if len(results) != 0:
-                    self.choose_car(results)
-                    choice = int(input("> Select a car: "))
-                    chosen_car = results[choice-1]
-                    created_car = self.car_serv.create_car(chosen_car)
-                    rent_status = self.car_repo.car_model.get_status(created_car)
-
-                    if rent_status == "False":
-                        print("This car is not available for renting. Please find another.\n")
-                    elif rent_status == "True":
-                        result = self.car_serv.rent_car(created_car)
-                        self.car_repo.remove_car(plate)
-                        changed_status = self.car_serv.create_car_from_list(result)
-                        self.car_repo.add_car(changed_status)
-                        print("Complete! The car has been updated to an 'unavailable' status!\n")
-
+                valid_car = True
+                print("New car will be added into the database.\n")
+                choose_veh_type = input("Available vehicle types:\n1. Sedan\n2. SUV\n3. MPV\n4. Mini\n5. Sport\n> Choose a type by its name or number: ")
+                if (choose_veh_type == "Sedan") or (choose_veh_type == "1"):
+                    veh_type = "Sedan"
+                elif (choose_veh_type == "SUV") or (choose_veh_type == "2"):
+                    veh_type = "SUV"
+                elif (choose_veh_type == "MPV") or (choose_veh_type == "3"):
+                    veh_type = "MPV"
+                elif (choose_veh_type == "Mini") or (choose_veh_type == "4"):
+                    veh_type = "Mini"
+                elif (choose_veh_type == "Sport") or (choose_veh_type == "5"):
+                    veh_type = "Sport"          
                 else:
-                    print("No match.\n")
+                    print("Failed")
+                    valid_car = False
+                # Campagna, Suzuki, Ferrari, Audi, Unique
+                choose_brand = input("\nAvailable car brands:\n1. Campagna\n2. Suzuki\n3. Ferrari\n4. Audi\n5. Unique\n> Choose a brand by its name or number: ")
+                if (choose_veh_type == "Campagna") or (choose_veh_type == "1"):
+                    brand = "Campagna"
+                elif (choose_veh_type == "Suzuki") or (choose_veh_type == "2"):
+                    brand = "Suzuki"
+                elif (choose_veh_type == "Ferrari") or (choose_veh_type == "3"):
+                    brand = "Ferrari"
+                elif (choose_veh_type == "Audi") or (choose_veh_type == "4"):
+                    brand = "Audi"
+                elif (choose_veh_type == "Unique") or (choose_veh_type == "5"):
+                    brand = "Unique"    
+                else:
+                    print("Failed")
+                    valid_car = False
+
+                plate = input("\nWhat is the car plate? License plates should have 3 letters followed by 2 numbers\n> Enter car plate here: ")
+
+                print("\nAvailable wheel drives:\n1. 4 Wheel Drive (4WD)\n2. Forward Wheel Drive (FWD)\n3. Rear Wheel Drive (RWD)")
+                choose_wheel_drive = input("> Choose a wheel drive by number, full name or abbreviation: ")
+                if (choose_wheel_drive == "1") or (choose_wheel_drive == "4 Wheel Drive") or (choose_wheel_drive == "4WD"):
+                    wheel_drive = "4WD"
+                elif (choose_wheel_drive == "2") or (choose_wheel_drive == "Forward Wheel Drive") or (choose_wheel_drive == "FWD"):
+                    wheel_drive = "FWD"
+                elif (choose_wheel_drive == "3") or (choose_wheel_drive == "Rear Wheel Drive") or (choose_wheel_drive == "RWD"):
+                    wheel_drive = "RWD"
+                else:
+                    print("Failed")                    
+                    valid_car = False
+
+                status = "True"
+
+                choose_is_manual = input("\n> Is the car:\n1 Manual (M)\n2. Automatic (A)\n> Answer here: ")
+                if (choose_is_manual.upper() == "MANUAL") or (choose_is_manual == "MANUAL") or (choose_is_manual == "Manual") or (choose_is_manual == "1") or (choose_is_manual == "M"):
+                    is_manual = "MANUAL"
+                elif (choose_is_manual.upper() == "AUTOMATIC") or (choose_is_manual == "AUTOMATIC") or (choose_is_manual == "Automatic") or (choose_is_manual == "2") or (choose_is_manual == "A"):
+                    is_manual = "AUTOMATIC"
+                else:
+                    print("Failed")
+                    valid_car = False
+
+                try:
+                    driven = int(input("\nHas the car been driven before?\n> Answer in integers and in KM: "))
+                    if driven < 0:
+                        print("Failed")
+                        valid_car = False
+                except Exception:
+                    print("Failed")
+                    valid_car = False
+                
+                choose_fuel_type = input("\nAvailable fuel types:\n1. Gazolene (G)\n2. Diesel (D)\n> Answer here: ")
+                if (choose_fuel_type == "1") or (choose_fuel_type == "G") or (choose_fuel_type == "Gazolene") or (choose_fuel_type == "gazolene") or (choose_fuel_type.upper() == "GAZOLENE"):
+                    fuel_type = "GAZOLENE"
+                elif (choose_fuel_type == "2" or "D") or (choose_fuel_type == "Diesel") or (choose_fuel_type == "diesel") or (choose_fuel_type.upper() == "DIESEL"):
+                    fuel_type = "DIESEL"
+                else:
+                    print("Failed")
+                    valid_car = False
+
+                if valid_car == True:
+                    price = self.car_serv.calculate_price(veh_type)
+                    print("\nVehicle Type: {}\nCar brand: {}\nLicense plate: {}\nWheel drive: {}\nAuto/Man: {}\nDriven: {}\nFuel type: {}\nCar Price: {}\n".format(veh_type, brand, plate, wheel_drive, is_manual, driven, fuel_type, price))
+                    confirmation = input("> Is this information correct?(Y/N)")
+                    if confirmation == "Y" or "Yes":
+                        new_car = self.car_repo.car_model(veh_type, brand, plate, wheel_drive, status, is_manual, driven, fuel_type, price)
+                        self.car_repo.add_car(new_car) # Þarf ennþá að láta gá hvort bíll sé inn í database-i, endilega laga við tækifæri
+                        print("The car has been successfully updated into the car database")
+                    elif confirmation == "N" or "No":
+                        print("Operation cancelled, going back to Cars section...\n")
+                else: 
+                    print("Something went wrong! Did you input all information correctly?\n")
+                # self.veh_type, self.brand, self.plate, self.wheel_drive, self.status, self.is_manual, self.driven, self.fuel_type, self.price
 
             elif choice == "2":
-                plate = input("> Enter the license plate of desired car: ")
-                results = self.car_repo.find_car(plate)
-
-                if len(results) != 0:
-                    self.choose_car(results)
-                    choice = int(input("> Select a car: "))
-                    chosen_car = results[choice-1]
-                    created_car = self.car_serv.create_car(chosen_car)
-                    rent_status = self.car_repo.car_model.get_status(created_car)
-
-                    if rent_status == "True":
-                        print("This car is available and doesn't need returning.\n")
-                    elif rent_status == "False":
-                        result = self.car_serv.rent_car(created_car)
-                        self.car_repo.remove_car(plate)
-                        changed_status = self.car_serv.create_car_from_list(result)
-                        self.car_repo.add_car(changed_status)
-                        print("Complete! The car has been returned and is 'available' again!\n")
-
-                else:
-                    print("No match.\n")
-
-            elif choice == "3":                
-                searchword = input("> Insert information as search word; Vehicle type, Brand, License plate, etc\nPlease only enter a single item:  ")
+                searchword = input("\n> Insert information as search word; Vehicle type, Brand, License plate, etc\nPlease only enter a single item:  ")
                 results = self.car_repo.find_car(searchword)
                 if len(results) == 0:
-                    print("No results for this search word.")
+                    print("No results for this search word.\n")
+                else:
+                    self.choose_car(results)
+                    choice = input("> Select car to remove (Write its license plate), or write 'C' to cancel: ")
+                    if (choice == "c") or (choice == "C"):
+                        print("Operation cancelled.")
+                    else:
+                        self.car_repo.remove_car(choice)
+                        print("Car successfully removed.")
+
+            elif choice == "3":                
+                searchword = input("\n> Insert information as search word; Vehicle type, Brand, License plate, etc\nPlease only enter a single item:  ")
+                results = self.car_repo.find_car(searchword)
+                if len(results) == 0:
+                    print("No results for this search word.\n")
                 else:
                     self.choose_car(results)
 
@@ -91,13 +145,6 @@ class Car_UI(object):
                 print(self.car_serv.get_all_cars())
             
             elif choice != "q":
-                print("Invalid input! Please enter the number/letter in front of each operation!")
+                print("Invalid input! Please enter the number/letter in front of each operation!\n")
 
-
-    def order_menu(self):
-        print("Available car:\n")
-        available_cars = self.car_serv.get_available_cars_list()
-        chosen_car = self.choose_car(available_cars)
-        rented_car = self.car_serv.create_car(chosen_car)
-        return rented_car
-            
+        print("Going back to main menu...\n")
