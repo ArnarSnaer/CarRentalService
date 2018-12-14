6a25479b3cce4e2c37b8d86aa16d63b7784c77ec
Bjarni <bjarnib18@ru.is>
2018-12-14 13:51:25 +0000

    Breytingar á ýmsu í client ui, services og repo. Aðallega verið að eyða print föllum og gerði nýtt validation fall

--- N E W/Main/main.py
+++ N E W/Main/main.py
@@ -13,22 +13,6 @@ def main():
         os.system('cls')
         print("Available sections:\n1. Order\n2. Cars\n3. Client\n4. Calculator\nq. Exit program")
         choice = input("> Choose a section: ")
-<<<<<<< HEAD
-        if (choice == "1") or (choice == "Order"):
-            order_UserInterface = Order_UI()
-            order_UserInterface.order_menu()
-        elif (choice == "2") or (choice == "Cars"):
-            car_UserInterface = Car_UI()
-            car_UserInterface.car_menu()
-        elif (choice == "3") or (choice == "Client"):
-            client_UserInterface = Client_ui()
-            client_UserInterface.main_menu()
-        elif (choice == "4") or (choice == "Calculator"):
-            calc_UserInterface = Calc_UI()
-            calc_UserInterface.calc_menu()
-        elif (choice == "q") or (choice == "Q"):
-            print("Exiting program...\n")
-=======
         if choice == "1":
             os.system('cls')
             order_UserInterface = Order_UI()
@@ -47,7 +31,6 @@ def main():
             calc_UserInterface.calc_menu()      
         elif choice =="q" or choice=="Q":
             print("-"*86,"\nExiting program...\n")
->>>>>>> a9603752a19f47ba9ed2bc408e183decfb937c22
             break
         else:
             print("Invalid input!")   
